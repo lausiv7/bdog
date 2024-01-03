4926078d9c7b32c3428963e6604f526d9a080414
unnie <lausiv@gmail.com>
2024-01-04 07:41:13 +0900

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,7 +1,7 @@
 // 注: process.env.XX是Vercel的环境变量，配置方式见：https://docs.tangly1024.com/article/how-to-config-notion-next#c4768010ae7d44609b744e79e2f9959a
 const BLOG = {
   // Important page_id！！！Duplicate Template from  https://www.notion.so/tanghh/02ab3b8678004aa69e9e415905ef32a5
-  // https://www.notion.so/DTOK-Decentrallized-Talk-GPT-5d1703874381445ea96b36b76a98965a?pvs=4 ㄴ
+  // https://www.notion.so/DTOK-Decentrallized-Talk-GPT-5d1703874381445ea96b36b76a98965a?pvs=4 ㄴㄴ
   NOTION_PAGE_ID:
         process.env.NOTION_PAGE_ID || 'DTOK-Decentrallized-Talk-GPT-5d1703874381445ea96b36b76a98965a',
   PSEUDO_STATIC: process.env.NEXT_PUBLIC_PSEUDO_STATIC || false, // 伪静态路径，开启后所有文章URL都以 .html 结尾。
